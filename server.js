c2d2e071f7a750396c15d99fb7ebd45b75cc986a
Uday Kiran <udaygam@gmail.com>
2019-09-15 17:26:41 +0530

    Initial Commit

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const app = express();
+
+app.get("/", (req, res) => {
+  res.json({
+    Message: "Running on Port 3030"
+  });
+});
+
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
+  console.log(`Server started on ${port}`);
+});
